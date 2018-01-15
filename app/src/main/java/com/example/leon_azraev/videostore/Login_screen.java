868aec6eb6d6a71334acbde40a0f9f03a0dbf69b
leonazraev <lazraev@gmail.com>
2018-01-15 13:00:37 +0200

    manager login

--- app/src/main/java/com/example/leon_azraev/videostore/Login_screen.java
+++ app/src/main/java/com/example/leon_azraev/videostore/Login_screen.java
@@ -2,8 +2,8 @@
 package com.example.leon_azraev.videostore;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,14 +17,14 @@ import com.google.firebase.database.ValueEventListener;
 
 
 public class Login_screen extends AppCompatActivity {
+    public static int count1 = 0;
+    public static int count2 = 0;
     public Button register_;
     public Button submit;
     public EditText user_name;
     public EditText password;
     private DatabaseReference userDB;
     private DatabaseReference userDB2;
-
-
 
     public void Login_screen_to_Registration() {
         register_ = findViewById(R.id.register_);
@@ -50,138 +50,109 @@ public class Login_screen extends AppCompatActivity {
                         password=(EditText) findViewById(R.id.password_);
                         String u=user_name.getText().toString();
                         String p=password.getText().toString();
-                        int count =0;
+                        count1 = 0;
+
                         for (DataSnapshot k:dataSnapshot.getChildren()) {
 
                             if (k.getKey().equals("userName")) {
                                 if (u.equals(k.getValue().toString())) {
-                                    count++;
+                                    count1++;
                                 }
                             }
                             if (k.getKey().equals("password")) {
                                 if (p.equals(k.getValue().toString())) {
-                                    count++;
+                                    count1++;
                                 }
                             }
 
                         }
 
-                        if(count==2) {
-                            Intent intent = new Intent(Login_screen.this, home_page_manager.class);
+                        if (count1 == 2) {
+                            Intent intent = new Intent(Login_screen.this, homepageactivity.class);
                             startActivity(intent);
-                            count = 0;
                             finish();
                             return;
                         }
-
-
                     }
-
                     @Override
                     public void onCancelled(DatabaseError databaseError) {
 
                     }
                 });
+                if (count1 != 2) {
+                    userDB.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            User usr = new User();
+                            user_name = (EditText) findViewById(R.id.user_name_);
+                            password = (EditText) findViewById(R.id.password_);
+                            String u = user_name.getText().toString();
+                            String p = password.getText().toString();
+                            count2 = 0;
+                            for (DataSnapshot k : dataSnapshot.getChildren()) {
+                                count2 = 0;
+                                for (DataSnapshot d : k.getChildren()) {
 
-
-
-
-
-
-                userDB.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        User usr = new User();
-                        user_name=(EditText) findViewById(R.id.user_name_);
-                        password=(EditText) findViewById(R.id.password_);
-                        String u=user_name.getText().toString();
-                        String p=password.getText().toString();
-                        int count=0;
-                        for (DataSnapshot k:dataSnapshot.getChildren()) {
-
-                                for (DataSnapshot d:k.getChildren()) {
-
-                                if (d.getKey().equals("userName")) {
-                                    if (u.equals(d.getValue().toString())) {
-                                        count++;
+                                    if (d.getKey().equals("userName")) {
+                                        if (u.equals(d.getValue().toString())) {
+                                            count2++;
+                                        }
                                     }
-                                }
-                                if (d.getKey().equals("password")) {
-                                    if (p.equals(d.getValue().toString())) {
-                                        count++;
+                                    if (d.getKey().equals("password")) {
+                                        if (p.equals(d.getValue().toString())) {
+                                            count2++;
+                                        }
                                     }
-                                }
-                                if(count==2)
-                                {
-                                    for (DataSnapshot m:k.getChildren())
-                                    {
-                                        if(m.getKey().equals("userName"))
-                                        {
-                                            usr.setUserName(m.getValue().toString());
-                                        }
-                                        if(m.getKey().equals("password"))
-                                        {
-                                            usr.setPassword(m.getValue().toString());
-                                        }
-                                        if(m.getKey().equals("firstName"))
-                                        {
-                                            usr.setFirstName(m.getValue().toString());
-                                        }
-                                        if(m.getKey().equals("lastName"))
-                                        {
-                                            usr.setLastName(m.getValue().toString());
-                                        }
-                                        if(m.getKey().equals("street"))
-                                        {
-                                            usr.setStreet(m.getValue().toString());
-                                        }
-                                        if(m.getKey().equals("city"))
-                                        {
-                                            usr.setCity(m.getValue().toString());
-                                        }
-                                        if(m.getKey().equals("email"))
-                                        {
-                                            usr.setEmail(m.getValue().toString());
-                                        }
+                                    if (count2 == 2) {
+                                        for (DataSnapshot m : k.getChildren()) {
+                                            if (m.getKey().equals("userName")) {
+                                                usr.setUserName(m.getValue().toString());
+                                            }
+                                            if (m.getKey().equals("password")) {
+                                                usr.setPassword(m.getValue().toString());
+                                            }
+                                            if (m.getKey().equals("firstName")) {
+                                                usr.setFirstName(m.getValue().toString());
+                                            }
+                                            if (m.getKey().equals("lastName")) {
+                                                usr.setLastName(m.getValue().toString());
+                                            }
+                                            if (m.getKey().equals("street")) {
+                                                usr.setStreet(m.getValue().toString());
+                                            }
+                                            if (m.getKey().equals("city")) {
+                                                usr.setCity(m.getValue().toString());
+                                            }
+                                            if (m.getKey().equals("email")) {
+                                                usr.setEmail(m.getValue().toString());
+                                            }
 
-                                    }
-                                    if(count==2) {
-
+                                        }
                                         Intent intent = new Intent(Login_screen.this, HomePage.class);
-
                                         intent.putExtra("myUSER", usr);
                                         startActivity(intent);
-                                        count = 0;
                                         finish();
-
                                         return;
                                     }
                                 }
+                                if (count2 == 2)
+                                    break;
+                            }
+                            if (count2 != 2 && count1 != 2) {
+                                Toast.makeText(Login_screen.this, "No such user in the system", Toast.LENGTH_SHORT).show();
 
                             }
-
-
-                        }
-                        if (count != 2) {
-                            Toast.makeText(Login_screen.this, "No such user in the system", Toast.LENGTH_SHORT).show();
                         }
 
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
 
-                    }
-
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-
-                    }
-                });
-
-
+                        }
+                    });
+                }
             }
         });
     }
-
-
-
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/leon_azraev/videostore/homepageactivity.java
+++ app/src/main/java/com/example/leon_azraev/videostore/homepageactivity.java
@@ -3,11 +3,11 @@ package com.example.leon_azraev.videostore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class home_page_manager extends AppCompatActivity {
+public class homepageactivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_page_manager);
+        setContentView(R.layout.activity_homepageactivity);
     }
 }
